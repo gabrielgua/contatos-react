456db726793ec099fb0702ced622ff8dbaad8d65
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-09-02 15:44:47 -0300

    feat: adding search by email or fullname funcionality

--- src/containers/Contacts/index.tsx
+++ src/containers/Contacts/index.tsx
@@ -1,20 +1,39 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Contact from '../../components/Contact'
 import { RootReducer } from '../../store'
 import { Button, Input } from '../../styles'
 import ContactsHeader, { ContactsList } from './styles'
+import { useState } from 'react'
+import { changeTerm } from '../../store/reducers/filter'
 
 const Contacts = () => {
+  const dispatch = useDispatch()
   const contacts = useSelector((state: RootReducer) => state.contacts.items)
+  const term = useSelector((state: RootReducer) => state.filter.term)
+
+  const filteredContacts = () => {
+    if (!term.length) {
+      return contacts
+    }
+
+    return contacts.filter(
+      (contact) =>
+        contact.fullName.toLowerCase().search(term) >= 0 ||
+        contact.email.toLowerCase().search(term) >= 0
+    )
+  }
 
   return (
     <>
       <ContactsHeader>
         <Button>Adicionar +</Button>
-        <Input placeholder="Buscar contatos" />
+        <Input
+          placeholder="Buscar contatos"
+          onChange={(e) => dispatch(changeTerm(e.target.value))}
+        />
       </ContactsHeader>
       <ContactsList>
-        {contacts.map((contact) => (
+        {filteredContacts().map((contact) => (
           <Contact key={contact.id} contact={contact} />
         ))}
       </ContactsList>
--- src/store/reducers/contacts.ts
+++ src/store/reducers/contacts.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import Contacts from '../../containers/Contacts'
 import { Contact } from '../../types/contact'
 
 type ContactState = {
